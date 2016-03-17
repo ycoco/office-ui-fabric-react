92fb5ead12df96ef2f23eb6ef91b98143836d2eb
Harry Ferguson <harryf@microsoft.com>
2016-03-17 19:25:03 +0000

    PR 15125: Merge harryf/fixUpBower to master
     - Fixes up bower.json
     - Adds missing typings with odsp-shared dependency
     - Adds puid and anid to logs

--- src/local/Init.ts
+++ src/local/Init.ts
@@ -35,27 +35,29 @@ Manager.cleanString = (str: string) => {
     return str;
 };
 
+PageInit.init();
+
+// Initialize Aria
+let preProductionToken = "3d697adc11914fa4991f1c3870255788-1afd84c4-bb01-494a-bfb0-0216ab2771c6-6977";
+let productionToken = "a23e4f242c9c4097a968f28c62633e19-62d0d830-5afd-4df3-8e40-351c8711cf5c-7157";
+let isProduction = (window["Flight"].version !== 'dev' &&
+    !!window["Flight"].UserGroup &&
+    (window["Flight"].UserGroup.toLowerCase() === 'df' ||
+        window["Flight"].UserGroup.toLowerCase() === 'prod'));
+AriaLogger.Init(isProduction ? productionToken : preProductionToken,
+    {
+        isAuthenticated: PageConfig.isAuthenticated(),
+        market: PageConfig.getMarket(),
+        session: window["Flight"].session,
+        version: window["Flight"].version,
+        manifest: window["Flight"].manifest,
+        userId: PageConfig.isAuthenticated() ? window["Flight"].cid : window["Flight"].xid,
+        userMsaId: window["$Config"] && window["$Config"].upsellUserId,
+        userANID: PageConfig.getAnid(),
+        environment: `ODC_${window["Flight"].UserGroup}`,
+        workload: 'ODC'
+    });
+
 export default function init() {
     "use strict";
-
-    PageInit.init();
-
-    // Initialize Aria
-    let preProductionToken = "3d697adc11914fa4991f1c3870255788-1afd84c4-bb01-494a-bfb0-0216ab2771c6-6977";
-    let productionToken = "a23e4f242c9c4097a968f28c62633e19-62d0d830-5afd-4df3-8e40-351c8711cf5c-7157";
-    let isProduction = (window["Flight"].version !== 'dev' &&
-        !!window["Flight"].UserGroup &&
-        (window["Flight"].UserGroup.toLowerCase() === 'df' ||
-            window["Flight"].UserGroup.toLowerCase() === 'prod'));
-    AriaLogger.Init(isProduction ? productionToken : preProductionToken,
-        {
-            isAuthenticated: PageConfig.isAuthenticated(),
-            market: PageConfig.getMarket(),
-            session: window["Flight"].session,
-            version: window["Flight"].version,
-            manifest: window["Flight"].manifest,
-            userId: PageConfig.isAuthenticated() ? window["Flight"].cid : window["Flight"].xid,
-            environment: `ODC_${window["Flight"].UserGroup}`,
-            workload: 'ODC'
-        });
 }
