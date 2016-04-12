b411293f0a6b2ce2276bea6c0a9b879addd087f9
xiujuan wang <xiujwang@microsoft.com>
2016-04-12 15:59:54 -0700

    Move RUMone framework to ODSP-Utilities

---
+++ src/local/logging/rumone/APICallPerformanceData.ts
@@ -0,0 +1,26 @@
+﻿// OneDrive:IgnoreCodeCoverage
+
+class APICallPerformanceData {
+    url: string;
+    duration: number;
+    correlationId: string;
+    status: number;
+    startTime: string;
+    endTime: string;
+    constructor(
+        url: string,
+        duration: number,
+        correlationid: string,
+        status: number,
+        startTime: string,
+        endTime: string) {
+        this.url = url;
+        this.duration = duration;
+        this.correlationId = correlationid;
+        this.status = status;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
+
+export = APICallPerformanceData;
