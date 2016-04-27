bb00a60515ce062908353f2a19b587d4e7d21c8b
Loredana Gavrilescu <loreg@microsoft.com>
2016-04-27 21:13:32 +0000

    PR 17820: Merge loreg/msit to master
     - added tenant for MSIT

--- src/local/SPOInit.ts
+++ src/local/SPOInit.ts
@@ -7,7 +7,7 @@ export default function init(workloadName: string) {
         workload: workloadName,
         tokens: {
             preProduction: "a220feed3a784dadaffa8496d2ef0fd7-4de4a2a0-a8e2-4ce3-8921-621c5304a5f7-7565",
-            msit: "",
+            msit: "3620e5572cf04917bf4993285ebe4e15-d5c60963-35a1-45e2-9430-928e0656b9cd-6718",
             prod: ""
         }
     });
