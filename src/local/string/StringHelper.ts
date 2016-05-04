569bbdf80ca35185e523653b7f8db8a45dd14bd8
Junle Li <junlel@microsoft.com>
2016-05-04 18:05:30 +0000

    Remove pluralSelect function requested by Anton.

--- src/local/string/StringHelper.ts
+++ src/local/string/StringHelper.ts
@@ -128,17 +128,6 @@ class StringHelper {
     }
 
     /**
-     * Selects a string based on plurality
-     * @param {number} count - The value to base selection on
-     * @param {string} single - The string to select when it's a singular value
-     * @param {string} multiple - The string to select when it's a plural value
-     * @returns {string}
-     */
-    public static pluralSelect(count: number, single: string, plural: string) {
-        return count === 1 ? single : plural;
-    }
-
-    /**
      * Return a string of the given length, using 0s to pad in from the right.
      */
     public static rightPad(data: any, length: number): string {
