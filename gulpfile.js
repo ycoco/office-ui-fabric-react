6bb021f1f0d455d0e1c71f4ce3b246173c69f64a
Harry Ferguson <harryf@microsoft.com>
2016-05-03 17:39:43 -0700

    Fixes up the build to use inline-bundle

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@
 global.__gulpRoot = __dirname;
 
 // Add log at the top for more timing information
-var log = require('onedrive-buildtools/log');
+var log = require('@ms/onedrive-buildtools/log');
 var gulp = require('gulp');
-var setupOneJsBuild = require('onedrive-buildtools/odbuild/setup-onejs-build');
+var setupOneJsBuild = require('@ms/onedrive-buildtools/odbuild/setup-onejs-build');
 
 // Setup one js build
 var gulpTasksPaths = setupOneJsBuild.getGulpTasksPaths();
@@ -21,7 +21,8 @@ var buildOptions = {
     gulpTaskOptions: {
         // Build bundles/manifests before running tests to fix issue where tests
         // would fail due to timeouts when running in VSO
-        'test': ['build-manifests']
+        'test': ['build-manifests'],
+        'default': ['inline-bundle']
     },
     
     // Tell gulp-onejs-build that our dist branch is separate from master
