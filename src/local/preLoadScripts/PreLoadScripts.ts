a5dd4f39de06c91cfc4274179223e3cecfe2ec6d
Harry Ferguson <harryf@microsoft.com>
2016-05-31 13:01:45 -0700

    Make sure we always add the scripts after the require script tag

--- src/local/preLoadScripts/PreLoadScripts.ts
+++ src/local/preLoadScripts/PreLoadScripts.ts
@@ -3,21 +3,48 @@
 
 export default function preLoad(baseUrl: string, paths: string[]) {
     "use strict";
+    let attempts = 0;
+
+    function preloadCallback() {
+        if (attempts < 100) {
+            let foundRequire = false;
+
+            // look at all script sources for require
+            let scripts = document.scripts;
+            if (scripts) {
+                for (let x = 0; x < scripts.length; x++) {
+                    let script = scripts[x];
+                    if (script['src'] && script['src'].indexOf('require-')) {
+                        foundRequire = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!foundRequire) {
+                /* tslint:disable:ban-native-functions */
+                setTimeout(preloadCallback, 1/* use none zero value due to IE optimizations */);
+                /* tslint:enable:ban-native-functions */
+            } else {
+                if (!window['define'] && paths) {
+                    console.log(`Prefetching ${paths.length} JS files`);
+                    for (let path of paths) {
+                        let url = `${baseUrl}${path}.js`;
+                        let script = document.createElement('script');
+                        script.src = url;
+                        script.async = true;
+                        document.head.appendChild(script);
+                    }
+                }
+            }
+
+            attempts++;
+        }
+    }
 
     if (paths) {
         /* tslint:disable:ban-native-functions */
-        setTimeout(function () {
-            /* tslint:enable:ban-native-functions */
-            if (!window['define'] && paths) {
-                console.log(`Prefetching ${paths.length} JS files`);
-                for (let path of paths) {
-                    let url = `${baseUrl}${path}.js`;
-                    let script = document.createElement('script');
-                    script.src = url;
-                    script.async = true;
-                    document.head.appendChild(script);
-                }
-            }
-        }, 0);
+        setTimeout(preloadCallback, 0);
+        /* tslint:enable:ban-native-functions */
     }
 }
