83db64e24beed4299cd094bb1caa818a0ae054a7
Harry Ferguson <harryf@microsoft.com>
2016-06-08 17:17:19 +0000

    PR 20838: Whitelists the browsers we will preload JS on
     - Whitelists the browsers we will preload JS on

--- src/local/preLoadScripts/PreLoadScripts.ts
+++ src/local/preLoadScripts/PreLoadScripts.ts
@@ -7,21 +7,36 @@ export default function preLoad(baseUrl: string, paths: string[]) {
 
     if (paths) {
         let platform = new PlatformDetection();
-        let useImage = !platform.isIE && !platform.isEdge;
 
-        console.log(`Prefetching ${paths.length} JS files with ${useImage ? 'image preloading' : 'with script tags'}`);
-        for (let path of paths) {
-            let url = `${baseUrl}${path}.js`;
+        let forceJSPreload = false;
 
-            if (useImage) {
-                let image = new Image();
-                image.src = url;
-            } else {
-                let script = document.createElement('script');
-                script.src = url;
-                script.type = "text/cache";
-                script.async = true;
-                document.head.appendChild(script);
+        if (DEBUG) {
+            forceJSPreload = document.location.href.indexOf('forceJSPreload') > 0;
+        }
+
+        // Whitelist supported browsers for preload
+        let enablePreload = platform.isChrome || platform.isIE || platform.isEdge || forceJSPreload;
+
+        if (enablePreload) {
+            let useImage = !platform.isIE && !platform.isEdge;
+
+            if (DEBUG) {
+                console.log(`Prefetching ${paths.length} JS files with ${useImage ? 'image preloading' : 'with script tags'}`);
+            }
+
+            for (let path of paths) {
+                let url = `${baseUrl}${path}.js`;
+
+                if (useImage) {
+                    let image = new Image();
+                    image.src = url;
+                } else {
+                    let script = document.createElement('script');
+                    script.src = url;
+                    script.type = "text/cache";
+                    script.async = true;
+                    document.head.appendChild(script);
+                }
             }
         }
     }
