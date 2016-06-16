f562c3c6972236597f947a75c73fc6c60cf1795e
Thomas Michon <tmichon@microsoft.com>
2016-06-16 15:15:38 -0700

    Register Knockout templates in <head> instead of <body>

--- src/local/utilities/knockout/ComponentFactory.ts
+++ src/local/utilities/knockout/ComponentFactory.ts
@@ -1,35 +1,46 @@
 // OneDrive:IgnoreCodeCoverage
 
 import ko = require('knockout');
-import IComponentDefinition = require('./IComponentDefinition');
-import ViewModel = require("../../base/ViewModel");
+import ViewModel = require('odsp-shared/base/ViewModel');
 import DomUtils from 'odsp-utilities/domUtils/DomUtils';
 
-class ComponentFactory {
-    static registerComponent<VM extends ViewModel>(component: IComponentDefinition<VM>): IComponentDefinition<VM> {
-        ko.components.register(
-            component.tagName,
-            {
-                viewModel: component.viewModel,
-                template: component.template,
-                // True injects synchronously if already loaded, otherwise still async
-                // Default it to false
-                synchronous: component.synchronous || false
-            });
-
-        return component;
-    }
-
-    static registerTemplate(id: string, template: string) {
-        var scriptElement = document.createElement('script');
-
-        scriptElement.setAttribute('type', 'text/html');
-        scriptElement.id = id;
-
-        DomUtils.setText(scriptElement, template);
-
-        document.body.appendChild(scriptElement);
-    }
+export interface IComponentDefinition<VM extends ViewModel> {
+    tagName: string;
+    template: any;
+    viewModel?: new (params: any) => VM;
+    synchronous?: boolean;
 }
 
-export = ComponentFactory;
+export function registerComponent<VM extends ViewModel>(component: IComponentDefinition<VM>): IComponentDefinition<VM> {
+    'use strict';
+
+    let {
+        tagName,
+        template,
+        viewModel,
+        synchronous = false
+    } = component;
+
+    ko.components.register(
+        tagName,
+        {
+            viewModel: viewModel,
+            template: template,
+            synchronous: synchronous
+        });
+
+    return component;
+}
+
+export function registerTemplate(id: string, template: string) {
+    'use strict';
+
+    let scriptElement = document.createElement('script');
+
+    scriptElement.setAttribute('type', 'text/html');
+    scriptElement.id = id;
+
+    DomUtils.setText(scriptElement, template);
+
+    document.head.appendChild(scriptElement);
+}
--- src/local/utilities/knockout/IComponentDefinition.ts
+++ src/local/utilities/knockout/IComponentDefinition.ts
@@ -1,14 +1,6 @@
+
 // OneDrive:IgnoreCodeCoverage
 
-/// <reference path="../../../knockout/knockout" />
-
-import ViewModel = require('../../base/ViewModel');
-
-interface IComponentDefinition<VM extends ViewModel> {
-    tagName: string;
-    template: any;
-    viewModel?: new (params: any) => VM;
-    synchronous?: boolean;
-}
+import { IComponentDefinition } from './ComponentFactory';
 
 export = IComponentDefinition;
