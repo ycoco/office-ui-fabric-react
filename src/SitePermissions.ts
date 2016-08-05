6c0dc35c318904d5eaa61c32a3afb8688637b36d
Alina Benedict <abene@microsoft.com>
2016-08-05 21:03:56 +0000

    PR 24948: adding bundle for SitePermissions
     - adding bundle for SitePermissions

---
+++ src/SitePermissions.ts
@@ -0,0 +1,6 @@
+// Everything related to the Site Permissions datasource
+
+export * from './dataSources/roleAssignments/ISPUser';
+export * from './dataSources/roleAssignments/SitePermissionsDataSource';
+export * from './dataSources/roleAssignments/ISitePermissionsDataSource';
+export * from './dataSources/roleAssignments/PrincipalType';
