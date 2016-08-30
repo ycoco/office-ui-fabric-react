5dd91a9741c1e3d16dc5a338db79fee9f3b2f6a9
Ina Teegan <inateeg@microsoft.com>
2016-08-30 17:39:50 +0000

    PR 26697: add ModuleLoader from odsp-next
     - add ModuleLoader from odsp-next so it can be used by odsp-shared-react and sp-client

---
+++ src/local/async/ModuleLoader.ts
@@ -0,0 +1,33 @@
+// OneDrive:IgnoreCodeCoverage
+
+import Promise from './Promise';
+import RequireHelper from './RequireHelper';
+
+export interface IModule<T> {
+    default?: T;
+}
+
+export default class ModuleLoader {
+    public static load<T>(path: string, sourceRequire: typeof require = require): Promise<T | IModule<T>> {
+        return RequireHelper.promise<T | IModule<T>>(sourceRequire, path);
+    }
+
+    public static loadDefault<T>(path: string, sourceRequire: typeof require = require, getModule?: (module: any) => T): Promise<T> {
+        return this.load(path, sourceRequire).then((result: T | IModule<T>) => {
+            return this.getDefaultValue(result, getModule);
+        });
+    }
+
+    private static getDefaultValue<T>(result: T | IModule<T>, getModule?: (module: any) => T): T {
+        if (!result) {
+            return <T>result;
+        }
+        let {
+            default: type = <T>result
+        } = <IModule<T>>result;
+        if (getModule && getModule(type)) {
+            type = <T>getModule(type);
+        }
+        return type;
+    }
+}
