ba66ec811517c584db27c13233747991331a136b
Eli Block <eliblock@microsoft.com>
2016-10-13 17:53:03 -0700

    Add AddRemotes.cmd helper script to setup remotes on newly cloned repo

---
+++ AddRemotes.cmd
@@ -0,0 +1,3 @@
+git remote add -f odsp-datasources https://onedrive.visualstudio.com/DefaultCollection/OneDriveWeb/_git/odsp-datasources
+git remote add -f odsp-shared-react https://onedrive.visualstudio.com/DefaultCollection/OneDriveWeb/_git/odsp-shared-react
+git remote add -f odsp-utilities https://onedrive.visualstudio.com/DefaultCollection/OneDriveWeb/_git/odsp-utilities
