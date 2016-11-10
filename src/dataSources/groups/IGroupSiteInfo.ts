7a071d6feef151fe08ed5908f989ff677546e2fb
Matt Mooty <mamooty@microsoft.com>
2016-11-10 17:34:24 +0000

    PR 33021: adding webtemplate parameter to creategroup
     - adding webtemplate parameter to creategroup

--- src/dataSources/groups/IGroupSiteInfo.ts
+++ src/dataSources/groups/IGroupSiteInfo.ts
@@ -1,29 +1,57 @@
 export interface IGroupSiteInfo {
-    // if the siteStatus is GroupSiteStatus.Ready, the url of this group site
+    /**
+     * if the siteStatus is GroupSiteStatus.Ready, the url of this group site
+     */
     siteUrl?: string;
 
-    // if the siteStatus is GroupSiteStatus.Ready, the url of this group site
+    /**
+     * if the siteStatus is GroupSiteStatus.Ready, the url of this group site
+     */
     documentsUrl?: string;
 
-    // if the siteStatus is GroupSiteStatus.Error, the error message
+    /**
+     * if the siteStatus is GroupSiteStatus.Error, the error message
+     */
     errorMessage?: string;
 
-    // the provisioning status of the site
+    /**
+     * the provisioning status of the site
+     */
     siteStatus?: GroupSiteStatus;
 }
 
 export enum GroupSiteStatus {
-    // the site does not exist and we aren't currently provisioning it
+    /**
+     * the site does not exist and we aren't currently provisioning it
+     */
     NotFound = 0,
 
-    // the site is provisioning
+    /**
+     * the site is provisioning
+     */
     Provisioning = 1,
 
-    // the site is ready
+    /**
+     * the site is ready
+     */
     Ready = 2,
 
-    // there was an error provisioning the site
+    /*
+     * there was an error provisioning the site
+     */
     Error = 3
+}
+
+export enum GroupWebTemplate {
+    /**
+     * Corresponds to GROUP#0 SPWebTemplate in SPOREL
+     */
+    Team = 0,
+
+    /**
+     * Corresponds to SITEPAGEPUBLISHING#0 SPWebTemplate in SPOREL
+     */
+    SitePagePublishing = 1
 }
 
 export default IGroupSiteInfo;
