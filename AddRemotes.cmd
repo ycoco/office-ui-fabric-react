626b8f777a85396be154b7addac8d495891911cb
Eli Block <eliblock@microsoft.com>
2016-11-16 15:09:59 -0800

    Update import repo helper scripts

--- AddRemotes.cmd
+++ AddRemotes.cmd
@@ -1,3 +1,7 @@
 git remote add -f odsp-datasources https://onedrive.visualstudio.com/DefaultCollection/OneDriveWeb/_git/odsp-datasources
 git remote add -f odsp-shared-react https://onedrive.visualstudio.com/DefaultCollection/OneDriveWeb/_git/odsp-shared-react
 git remote add -f odsp-utilities https://onedrive.visualstudio.com/DefaultCollection/OneDriveWeb/_git/odsp-utilities
+git remote add -f odsp-shared https://onedrive.visualstudio.com/DefaultCollection/OneDriveWeb/_git/odsp-shared
+git remote add -f spo-core https://onedrive.visualstudio.com/DefaultCollection/OneDriveWeb/_git/spo-core
+git remote add -f odc-core https://onedrive.visualstudio.com/DefaultCollection/OneDriveWeb/_git/odc-core
+git remote add -f web-bootloader https://onedrive.visualstudio.com/DefaultCollection/OneDriveWeb/_git/web-bootloader
