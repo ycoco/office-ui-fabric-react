26069ab3936ca18129fee15a875666e260b25f7d
Constantin Dan Stanciu <danst@microsoft.com>
2016-11-18 12:49:37 -0800

    cherry pick f238067e

--- odsp-datasources/src/dataSources/groups/GroupsDataSource.ts
+++ odsp-datasources/src/dataSources/groups/GroupsDataSource.ts
@@ -1,7 +1,7 @@
 import IGroupsDataSource from './IGroupsDataSource';
 import IMembership from './IMembership';
 import MembersList from './MembersList';
-import IPerson  from '../peoplePicker/IPerson';
+import IPerson from '../peoplePicker/IPerson';
 import IGroup from './IGroup';
 import DataSource from '../base/DataSource';
 import Promise from '@ms/odsp-utilities/lib/async/Promise';
@@ -168,15 +168,22 @@ export default class GroupsDataSource extends DataSource implements IGroupsDataS
             StringHelper.format(getGroupByIdUrlTemplate, group.id),
             'SP.Directory.DirectorySession');
 
-        const postData = () => JSON.stringify({
-            __metadata: {
-                type: 'SP.Directory.Group'
-            },
-            displayName: group.name,
-            description: group.description,
-            isPublic: group.isPublic,
-            classification: group.classification
-        });
+        const postData = () => {
+            let data: any = {
+                __metadata: {
+                    type: 'SP.Directory.Group'
+                },
+                displayName: group.name,
+                description: group.description,
+                isPublic: group.isPublic
+            };
+
+            if (group.classification) {
+                data.classification = group.classification;
+            }
+
+            return JSON.stringify(data);
+        };
 
         return this.getData<void>(
             restUrl,
@@ -297,8 +304,8 @@ export default class GroupsDataSource extends DataSource implements IGroupsDataS
             undefined,
             undefined,
             GET_USER_GROUPS_FBIRANKED_RETRIES).then(
-                (value: IGroup[]) => { return value; },
-                getUserGroupsFallback);
+            (value: IGroup[]) => { return value; },
+            getUserGroupsFallback);
     }
 
     /**
@@ -485,9 +492,9 @@ export default class GroupsDataSource extends DataSource implements IGroupsDataS
                 (responseFromServer: string) => {
                     let batchResponseResult: IDataBatchOperationResult = DataBatchOperationHelper.processBatchResponse(responseFromServer);
                     if (batchResponseResult.hasError) {
-                        error( batchResponseResult );
+                        error(batchResponseResult);
                     } else {
-                        complete( batchResponseResult );
+                        complete(batchResponseResult);
                     }
                 },
                 (errorFromServer: any) => {
