c54a6ec428e2c44ead1f9da67368ff18e672adc6
Bryan Watts <brwatt@microsoft.com>
2017-03-08 23:19:42 +0000

    Merged PR 45618: Adding more file types for text editor to handle

--- odsp-utilities/src/local/icons/FileTypeIconMap.ts
+++ odsp-utilities/src/local/icons/FileTypeIconMap.ts
@@ -9,7 +9,7 @@ let FileTypeIconMap = {
     'accdb': { extensions: ['accdb', 'mdb'] },
     'archive': { type: ItemType.Unknown, extensions: ['7z', 'ace', 'arc', 'arj', 'dmg', 'gz', 'iso', 'lzh', 'pkg', 'rar', 'sit', 'tgz', 'tar', 'rar', 'z'] },
     'audio': { type: ItemType.Unknown, extensions: ['aif', 'aiff', 'aac', 'amr', 'au', 'awb', 'dct', 'dss', 'dvf', 'flac', 'gsm', 'm4a', 'm4p', 'mid', 'mmf', 'mp3', 'ogg', 'oga', 'ra', 'rm', 'wav', 'wma', 'wv'] },
-    'code': { extensions: ['asp', 'bat', 'c', 'coffee', 'config', 'cpp', 'cs', 'cshtml', 'css', 'dat', 'fsx', 'h', 'inf', 'ini', 'java', 'js', 'json', 'less', 'log', 'manifest', 'master', 'nfo', 'php', 'prefs', 'preferences', 'ps', 'ps1', 'pss', 'py', 'rb', 'rdf', 'reg', 'ruby', 'sql', 'svn-base', 'vmg', 'webpart', 'wsp', 'vb'] }, // separate code into code and sysText
+    'code': { extensions: ['Hcp', 'abap', 'ada', 'adp', 'ahk', 'as', 'as3', 'asc', 'ascx', 'asm', 'asp', 'awk', 'bash', 'bash_login', 'bash_logout', 'bash_profile', 'bashrc', 'bat', 'bib', 'bsh', 'build', 'builder', 'c', 'c++', 'capfile', 'cc', 'cfc', 'cfm', 'cfml', 'cl', 'clj', 'cls', 'cmake', 'cmd', 'coffee', 'coffee.erb', 'cpp', 'cpt', 'cpy', 'cs', 'cshtml', 'cson', 'csproj', 'css', 'css.erb', 'ctp', 'cxx', 'd', 'ddl', 'di', 'disco', 'dml', 'dtd', 'dtml', 'el', 'emakefile', 'erb', 'erl', 'f', 'f90', 'f95', 'fs', 'fsi', 'fsscript', 'fsx', 'gemfile', 'gemspec', 'gitconfig', 'go', 'groovy', 'gvy', 'h', 'h++', 'haml', 'handlebars', 'hbs', 'hh', 'hpp', 'hrl', 'hs', 'htc', 'hxx', 'idl', 'iim', 'inc', 'inf', 'ini', 'inl', 'ipp', 'irbrc', 'jade', 'jav', 'java', 'js', 'json', 'jsp', 'jsx', 'l', 'less', 'lhs', 'lisp', 'log', 'lst', 'ltx', 'lua', 'm', 'make', 'markdn', 'markdown', 'mdown', 'mkdn', 'ml', 'mli', 'mll', 'mly', 'mm', 'mud', 'nfo', 'opml', 'osascript', 'p', 'pas', 'patch', 'php', 'php2', 'php3', 'php4', 'php5', 'phtml', 'pl', 'pm', 'pod', 'pp', 'profile', 'ps', 'ps1', 'pt', 'py', 'pyw', 'r', 'rake', 'rb', 'rbx', 'rc', 're', 'reg', 'rest', 'resw', 'resx', 'rhtml', 'rjs', 'rprofile', 'rpy', 'rss', 'rst', 'rxml', 's', 'sass', 'scala', 'scm', 'sconscript', 'sconstruct', 'script', 'scss', 'sgml', 'sh', 'sh', 'shtml', 'sml', 'sql', 'sql', 'srf', 'sty', 'tcl', 'tex', 'textile', 'tld', 'tli', 'tmpl', 'tpl', 'vb', 'vi', 'vim', 'wsdl', 'xhtml', 'xoml', 'xsd', 'xslt', 'yaml', 'yaws', 'yml', 'zsh'] },
     'css': {}, // we dont have the icon yet, but i believe we want it, snapping to 'code' for now
     'csv': { extensions: ['csv'] },
     'docset': { type: ItemType.Folder, iconType: 'svg' },
@@ -20,7 +20,7 @@ let FileTypeIconMap = {
     'folder': { type: ItemType.Folder, iconType: 'svg' },
     'font': { type: ItemType.Unknown, extensions: ['ttf', 'otf', 'woff'] },
     'genericfile': { type: ItemType.Unknown },
-    'html': { type: ItemType.Unknown, extensions: ['htm', 'html', 'mht'] },
+    'html': { extensions: ['htm', 'html', 'mht'] },
     'link': { type: ItemType.Unknown, extensions: ['lnk', 'link', 'url', 'website', 'webloc'] },
     'listitem': { type: ItemType.Unknown, extensions: ['listitem'] },
     'model': { extensions: ['3ds', 'blend', 'dae', 'df', 'dwfx', 'dwg', 'dxf', 'layout', 'off', 'max', 'skp', 'stp', 't', 'thl', 'x'] },
@@ -42,7 +42,7 @@ let FileTypeIconMap = {
     'rtf': { extensions: ['epub', 'rtf', 'wri'] },
     'sharedfolder': { type: ItemType.Folder, iconType: 'svg' },
     'sysfile': { type: ItemType.Unknown, extensions: ['bak', 'bin', 'cab', 'cache', 'cat', 'cer', 'class', 'dat', 'db', 'dbg', 'dl_', 'dll', 'ithmb', 'jar', 'kb', 'kdc', 'ldt', 'lrprev', 'obj', 'ppa', 'ppam', 'pdb', 'rom', 'thm', 'thmx', 'vsl', 'xla', 'xlam', 'xll'] },
-    'txt': { extensions: ['md', 'readme', 'text', 'txt'] },
+    'txt': { extensions: ['dif', 'diff', 'md', 'markdown', 'out', 'plist', 'properties', 'readme', 'text', 'txt'] },
     'vector': { type: ItemType.Unknown, extensions: ['ai', 'dgn', 'pd', 'emf', 'eps', 'indd', 'indt', 'ps', 'svg', 'svgz', 'wmf'] },
     'video': { type: ItemType.Media, extensions: ['3gp', 'asf', 'avi', 'dvr-ms', 'flv', 'm1v', 'm4v', 'mkv', 'mod', 'mov', 'mm4p', 'mp2', 'mp2v', 'mp4', ' mpa', 'mpe', 'mpeg', 'mpg', 'mpv', 'mpv2', 'mts', 'ogg', 'qt', 'swf', 'ts', 'vob', 'webm', 'wlmp', 'wm', 'wmv', 'wmx'] },
     'vsdx': { extensions: ['vsd', 'vsdm', 'vsdx', 'vdw'] },
