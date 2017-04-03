d5f38f66774c69150e1ad60284d6e56d8eff5964
Stanley Yao <stanleyy@microsoft.com>
2017-04-03 16:54:19 +0000

    Merged PR 49370: For the renderers that have been moved to odsp-list-utilities, remove old copies from odsp-shared-react
    For the renderers that have been moved to odsp-list-utilities, remove old copies from odsp-shared-react

--- odsp-shared-react/src/components/Fields/DetailsList/FieldRenderers.ts
+++ odsp-shared-react/src/components/Fields/DetailsList/FieldRenderers.ts
@@ -1,7 +1,1 @@
-export { BaseText, IBaseTextProps } from './BaseText';
-export { DateRenderer, IDateRendererProps } from './DateRenderer';
-export { DotDotDotRenderer, IDotDotDotRendererProps } from './DotDotDotRenderer';
-export { FileTypeIconRenderer, IFileTypeIconRendererProps } from './FileTypeIconRenderer';
-export { NameRenderer, INameRendererProps } from './NameRenderer';
 export { TextRenderer, ITextRendererProps } from './TextRenderer';
-export { TitleRenderer, ITitleRendererProps } from './TitleRenderer';
