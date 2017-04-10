285bed649f62730ae4ae9894b849ef66fde4b735
Camille Malonzo <cmalonzo@microsoft.com>
2017-04-10 23:53:43 +0000

    Merged PR 51129: Expose CardList renderers (DocumentCardTile & TipTile)
    Expose CardList renderers to use in other scenarios and make sure cards are aligned along all of these

--- odsp-shared-react/src/components/CardList/index.ts
+++ odsp-shared-react/src/components/CardList/index.ts
@@ -1,2 +1,5 @@
 export * from './CardList';
 export * from './CardList.Props';
+export * from './renderers/DocumentCardTile';
+export * from './renderers/ICardTile.Props'
+export * from './renderers/TipTile';
