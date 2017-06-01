4cb0a88afc29bdf59f1827f9c6ab7356e630e6bb
Robert Chen <rchen@microsoft.com>
2017-06-01 01:25:49 +0000

    Merged PR 59888: support gradient for notes fields.
    support gradient for long notes fields---add fade effect. Will check in odsp-next part of the change after this is merged into ODSP-next.

--- odsp-list-utilities/src/Renderers/TextRenderer.tsx
+++ odsp-list-utilities/src/Renderers/TextRenderer.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { BaseText } from './BaseText';
 import Sanitize from '@ms/odsp-utilities/lib/encoding/Sanitize';
+import './TextRenderer.scss';
 
 export interface ITextRendererProps {
     /** Text to render */
@@ -15,17 +16,23 @@ export interface ITextRendererProps {
 
     /** aria label of the field. */
     ariaLabel?: string;
+
+    /** Apply special styling if an item is truncated */
+    isTruncated?: boolean;    
 }
 
 export function TextRenderer(props: ITextRendererProps) {
     'use strict';
 
-    let { text, isSafeToInnerHTML, isDisabled, ariaLabel } = props;
+    let { text, isSafeToInnerHTML, isDisabled, ariaLabel, isTruncated } = props;
     ariaLabel = ariaLabel || text;    // Default to text if no ariaLabel given
 
     if (isSafeToInnerHTML) {
         // apply special styling if item is disabled
         let textClass = 'od-FieldRenderer-text';
+        if (isTruncated) {
+            textClass += ' is-truncated';
+        }
         if (isDisabled) {
             textClass += ' od-FieldRenderer--disabled';
         }
