66d9accf6c384d806b1af5c6282f253ac6b596f2
joeli <joeli@microsoft.com>
2017-07-05 12:50:54 -0700

    joeli/AddEmailAuthGuestProperty

--- odsp-datasources/src/interfaces/ISpPageContext.ts
+++ odsp-datasources/src/interfaces/ISpPageContext.ts
@@ -194,6 +194,8 @@ export interface ISpPageContext {
     hasManageWebPermissions: boolean;
     /** True when the user is anonymous guest. */
     isAnonymousGuestUser?: boolean;
+    /** True when the user is email authentiction guest. */
+    isEmailAuthenticationGuestUser?: boolean;
     /** True when the user is external guest */
     isExternalGuestUser?: boolean;
     /** Whether the user is the admin of the site. */
