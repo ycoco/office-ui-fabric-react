d8e1acd5874ef7d2abb68f12d310fd0dcb735cd1
Stanley Yao <stanleyy@microsoft.com>
2017-07-06 06:44:15 +0000

    Merged PR 66543: Move the supported react field editor whitelist from odsp-next into odsp-shared-react to avoid the need of double check-ins to add a new editor
    The other half of the change is in odsp-next to consume this, which will be checked-in after this.

---
+++ odsp-shared-react/src/components/ClientForm/fieldEditor/SupportedFieldTypes.ts
@@ -0,0 +1,56 @@
+import { Engagement } from '@ms/odsp-utilities/lib/logging/events/Engagement.event';
+
+const supportedReactFieldEditorTypes: string[] = [
+    'Text',
+    'Boolean',
+    'Number',
+    'User',
+    'URL',
+    'Attachments'
+];
+
+function logUnsupportedFieldType(fieldType: string): void {
+    Engagement.logData({
+        name: "ReactClientForm.Fallback",
+        extraData: {
+            UnsupportedFieldType: fieldType
+        }
+    });
+}
+
+/**
+ * Given a ClientForm object, return 'true' if all field types have React client editor functionality
+ * available.  Otherwise, return 'false'.
+ */
+export function supportReactClientForm(clientForm: any): boolean {
+    let fields = clientForm && clientForm.fields || null;
+    if (fields === null || fields === undefined || !Array.isArray(fields)) {
+        // be conservative using the new ReactClientForm
+        // when in doubt, fall back to KO ClientForm
+        return false;
+    }
+
+    for (let idx = 0; idx < fields.length; idx++) {
+        let field = fields[idx];
+        if (field === undefined || field === null) {
+            // be conservative using the new ReactClientForm
+            // when in doubt, fall back to KO ClientForm
+            return false;
+        }
+        let fieldType: string = field && field.schema && field.schema.FieldType || '';
+        if (supportedReactFieldEditorTypes.indexOf(fieldType) < 0) {
+            // this field type doesn't have React field editor yet
+            logUnsupportedFieldType(fieldType);
+            return false;
+        }
+        if (fieldType === 'Choice') {
+            let fillInChoice: boolean = field && field.schema && field.schema.FillInChoice || true;
+            if (fillInChoice) {
+                logUnsupportedFieldType(fieldType);
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
