cdeb4d22ca3513e5b5ea53a71cbbb5aa0d0ebd0a
Joe Martella <joem@microsoft.com>
2017-07-16 18:35:54 +0000

    Merged PR 68165: Adds proper role to OutlookButton component
    - Minimizing code churn by applying ARIA role instead of refactoring component

--- odsp-shared-react/src/components/CompositeHeader/subComponents/OutlookButton.tsx
+++ odsp-shared-react/src/components/CompositeHeader/subComponents/OutlookButton.tsx
@@ -2,20 +2,20 @@ import * as React from 'react';
 import { IGoToOutlookProps } from '../CompositeHeader.Props';
 
 export const OutlookButton: React.StatelessComponent<IGoToOutlookProps> = (props: IGoToOutlookProps) => {
-  function _onGoToOutlookClick(ev: React.MouseEvent<HTMLElement>) {
-    if (props.goToOutlookAction) {
-      props.goToOutlookAction(ev);
-      ev.stopPropagation();
-      ev.preventDefault();
+    function _onGoToOutlookClick(ev: React.MouseEvent<HTMLElement>) {
+        if (props.goToOutlookAction) {
+            props.goToOutlookAction(ev);
+            ev.stopPropagation();
+            ev.preventDefault();
+        }
     }
-  }
 
-  return (
-    <span className='ms-compositeHeader-goToOutlook'>
-      <button className='ms-compositeHeaderButton' onClick={ _onGoToOutlookClick }>
-        <span className='ms-compositeHeader-goToOutlookText'>{ props.goToOutlookString }</span>
-        <i className='ms-compositeHeader-goToOutlookIcon ms-Icon ms-Icon--ArrowUpRight8'></i>
-      </button>
-    </span>
-  );
+    return (
+        <span className='ms-compositeHeader-goToOutlook'>
+            <button role='link' className='ms-compositeHeaderButton' onClick={ _onGoToOutlookClick }>
+                <span className='ms-compositeHeader-goToOutlookText'>{ props.goToOutlookString }</span>
+                <i className='ms-compositeHeader-goToOutlookIcon ms-Icon ms-Icon--ArrowUpRight8'></i>
+            </button>
+        </span>
+    );
 }
