17f1cbf288633652c236a031a860942542f17a3e
Philip Kuo <phkuo@microsoft.com>
2017-07-18 23:09:47 +0000

    Merged PR 68013: Package sp-css-api correctly
    Switch to the right build system.

--- sp-css-api/gulpfile.js
+++ sp-css-api/gulpfile.js
@@ -1,44 +1,20 @@
 'use strict';
 
-// Save the root globally so we can use it later
-global.__gulpRoot = __dirname;
+let build = require('@ms/odsp-build');
+let gulp = require('gulp');
 
-// Add log at the top for more timing information
-var log = require('@ms/onedrive-buildtools/log');
-var gulp = require('gulp');
-var path = require('path');
-var setupOneJsBuild = require('@ms/onedrive-buildtools/odbuild/setup-onejs-build');
+// initialize tasks.
+build.initialize(gulp);
 
-// Setup one js build
-var gulpTasksPaths = setupOneJsBuild.getGulpTasksPaths();
-var app = gulpTasksPaths.app.root;
+/** @todo: disable lint config. */
+build.tslint.setConfig({ lintConfig: require('./tslint.json') });
 
-var tsconfig = require('./tsconfig.json');
+// disable karma
+build.karma.setConfig({ configPath: null });
 
-var buildOptions = {
-    paths: {
-        linkedDeps: {
-            'node_modules/@ms/aria-private/dist/amd':                   path.join(app, '@ms', 'aria'),
-            'node_modules/@ms/odsp-utilities/dist/amd/odsp-utilities':  path.join(app, '@ms', 'odsp-utilities', 'lib')
-        },
-        types: {
-            typeRoots: tsconfig.compilerOptions.typeRoots,
-            defaultTypes: tsconfig.compilerOptions.types
-        }
-    },
+// Configure TypeScript.
+build.TypeScriptConfiguration.setTypescriptCompiler(require('typescript'));
 
-    // Mix in gulp task pre-reqs for tasks in gulp-onejs-build
-    gulpTaskOptions: {
-        // Build bundles/manifests before running tests to fix issue where tests
-        // would fail due to timeouts when running in VSO
-        'test': ['build-manifests']
-    },
-    // Tell gulp-onejs-build that our dist branch is separate from master
-    separateDistRepo: true,
+/** @todo: Enable css modules when ready. */
+// build.sass.setConfig({ useCSSModules: true });
 
-    nodeResolution: true
-};
-
-setupOneJsBuild.createGulpTasks(__dirname, gulp, buildOptions);
-
-log.markTaskCreationTime();
--- sp-css-api/src/local/GenerateCssApi.ts
+++ sp-css-api/src/local/GenerateCssApi.ts
@@ -4,118 +4,116 @@ import {
 import { format } from '@ms/odsp-utilities/lib/string/StringHelper';
 import Sanitize from '@ms/odsp-utilities/lib/encoding/Sanitize';
 
-export class GenerateCssApi {
 
-    /**
-     * Creates a style block that acts as a styling API. This API is made of themed CSS
-     * classes, and useful in scenarios where content creators can only author HTML.
-     * The naming scheme for the classes is:
-     * sp-css-[property being styled]-[slot name used for property's value]
-     * For example, if the bodyText slot is assigned the value #333333, these blocks
-     * (among others) would be generated:
-     * .sp-css-backgroundColor-bodyText { background-color: #333333; }
-     * .sp-css-color-bodyText { color: #333333; }
-     * .sp-css-borderColor-bodyText { border-color: #333333; }
-     */
-    public Generate() {
-        let theme = getTheme();
+/**
+ * Creates a style block that acts as a styling API. This API is made of themed CSS
+ * classes, and useful in scenarios where content creators can only author HTML.
+ * The naming scheme for the classes is:
+ * sp-css-[property being styled]-[slot name used for property's value]
+ * For example, if the bodyText slot is assigned the value #333333, these blocks
+ * (among others) would be generated:
+ * .sp-css-backgroundColor-bodyText { background-color: #333333; }
+ * .sp-css-color-bodyText { color: #333333; }
+ * .sp-css-borderColor-bodyText { border-color: #333333; }
+ */
+export function generateCssApi() {
+    let theme = getTheme();
 
-        const styleBlockId = 'spCssApi';
-        let originalStyleBlock = <HTMLStyleElement>document.getElementById(styleBlockId);
-        let styleBlock = originalStyleBlock || document.createElement("style");
-        if (!originalStyleBlock) {
-            styleBlock.type = 'text/css';
-            styleBlock.id = styleBlockId;
-        }
+    const styleBlockId = 'spCssApi';
+    let originalStyleBlock = <HTMLStyleElement>document.getElementById(styleBlockId);
+    let styleBlock = originalStyleBlock || document.createElement("style");
+    if (!originalStyleBlock) {
+        styleBlock.type = 'text/css';
+        styleBlock.id = styleBlockId;
+    }
 
-        const {
-            bodyBackground,
-            bodyText,
-            bodySubtext,
-            bodyDivider,
-            disabledBackground,
-            disabledText,
-            disabledSubtext,
-            errorBackground,
-            errorText,
-            listBackground,
-            listTextColor,
-            listItemBackgroundChecked
-        } = theme.semanticColors;
-        let standardSemanticSlots = {
-            bodyBackground,
-            bodyText,
-            bodySubtext,
-            bodyDivider,
-            disabledBackground,
-            disabledText,
-            disabledSubtext,
-            errorBackground,
-            errorText,
-            listBackground,
-            listTextColor,
-            listItemBackgroundChecked
-        };
+    const {
+        bodyBackground,
+        bodyText,
+        bodySubtext,
+        bodyDivider,
+        disabledBackground,
+        disabledText,
+        disabledSubtext,
+        errorBackground,
+        errorText,
+        listBackground,
+        listTextColor,
+        listItemBackgroundChecked
+    } = theme.semanticColors;
+    let standardSemanticSlots = {
+        bodyBackground,
+        bodyText,
+        bodySubtext,
+        bodyDivider,
+        disabledBackground,
+        disabledText,
+        disabledSubtext,
+        errorBackground,
+        errorText,
+        listBackground,
+        listTextColor,
+        listItemBackgroundChecked
+    };
 
-        const {
-            themeDarker,
-            themeDark,
-            themeDarkAlt,
-            themePrimary,
-            themeSecondary,
-            themeTertiary,
-            themeLight,
-            themeLighter,
-            themeLighterAlt,
-            black,
-            neutralDark,
-            neutralPrimary,
-            neutralPrimaryAlt,
-            neutralSecondary,
-            neutralTertiary,
-            neutralTertiaryAlt,
-            neutralQuaternary,
-            neutralQuaternaryAlt,
-            neutralLight,
-            neutralLighter,
-            neutralLighterAlt,
-            white
-        } = theme.palette;
-        let paletteSlots = {
-            themeDarker,
-            themeDark,
-            themeDarkAlt,
-            themePrimary,
-            themeSecondary,
-            themeTertiary,
-            themeLight,
-            themeLighter,
-            themeLighterAlt,
-            black,
-            neutralDark,
-            neutralPrimary,
-            neutralPrimaryAlt,
-            neutralSecondary,
-            neutralTertiary,
-            neutralTertiaryAlt,
-            neutralQuaternary,
-            neutralQuaternaryAlt,
-            neutralLight,
-            neutralLighter,
-            neutralLighterAlt,
-            white
-        };
+    const {
+        themeDarker,
+        themeDark,
+        themeDarkAlt,
+        themePrimary,
+        themeSecondary,
+        themeTertiary,
+        themeLight,
+        themeLighter,
+        themeLighterAlt,
+        black,
+        neutralDark,
+        neutralPrimary,
+        neutralPrimaryAlt,
+        neutralSecondary,
+        neutralTertiary,
+        neutralTertiaryAlt,
+        neutralQuaternary,
+        neutralQuaternaryAlt,
+        neutralLight,
+        neutralLighter,
+        neutralLighterAlt,
+        white
+    } = theme.palette;
+    let paletteSlots = {
+        themeDarker,
+        themeDark,
+        themeDarkAlt,
+        themePrimary,
+        themeSecondary,
+        themeTertiary,
+        themeLight,
+        themeLighter,
+        themeLighterAlt,
+        black,
+        neutralDark,
+        neutralPrimary,
+        neutralPrimaryAlt,
+        neutralSecondary,
+        neutralTertiary,
+        neutralTertiaryAlt,
+        neutralQuaternary,
+        neutralQuaternaryAlt,
+        neutralLight,
+        neutralLighter,
+        neutralLighterAlt,
+        white
+    };
 
-        let styles = '';
-        let standardSlots = { ...standardSemanticSlots, ...paletteSlots };
-        styles += this._makeBlocks('.sp-css-backgroundColor-{0}', 'background-color', standardSlots);
-        styles += this._makeBlocks('.sp-css-color-{0}', 'color', standardSlots);
-        styles += this._makeBlocks('.sp-css-borderColor-{0}', 'border-color', standardSlots);
+    let styles = '';
+    let standardSlots = { ...standardSemanticSlots, ...paletteSlots };
+    styles += _makeBlocks('.sp-css-backgroundColor-{0}', 'background-color', standardSlots);
+    styles += _makeBlocks('.sp-css-color-{0}', 'color', standardSlots);
+    styles += _makeBlocks('.sp-css-borderColor-{0}', 'border-color', standardSlots);
 
-        styleBlock.innerHTML = styles;
-        if (!originalStyleBlock) {
-            document.head.appendChild(styleBlock);
-        }
+    styleBlock.innerHTML = styles;
+    if (!originalStyleBlock) {
+        document.head.appendChild(styleBlock);
     }
 
     /**
@@ -129,7 +127,7 @@ export class GenerateCssApi {
      * @returns a string that contains all generated blocks, one for each entry in slotsToValues.
      * @memberof GenerateCssApi
      */
-    private _makeBlocks(
+    function _makeBlocks(
         selectorTemplate: string,
         property: string,
         slotsToValues: any
@@ -147,3 +145,4 @@ export class GenerateCssApi {
     }
 
 }
+
