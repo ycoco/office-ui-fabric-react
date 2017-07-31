417ede9190815dbbacad7e7e98b5339d0ef9e06d
Thomas Michon <tmichon@microsoft.com>
2017-07-31 16:13:52 -0700

    Normalize guids before sending in Engagement data

--- odsp-datasources/src/dataSources/engagement/handlers/PageContextEngagementHandler.ts
+++ odsp-datasources/src/dataSources/engagement/handlers/PageContextEngagementHandler.ts
@@ -1,6 +1,7 @@
 
 import { IEngagementHandler, IEngagementSingleSchema } from '@ms/odsp-utilities/lib/logging/engagement/Engagement';
 import ISpPageContext from '../../../interfaces/ISpPageContext';
+import Guid from '@ms/odsp-utilities/lib/guid/Guid';
 
 export interface IPageContextEngagementHandlerDependencies {
     pageContext: ISpPageContext;
@@ -15,17 +16,17 @@ export default class PageContextEngagementHandler implements IEngagementHandler 
 
     public getEngagementData(): Partial<IEngagementSingleSchema> {
         const engagementData: Partial<IEngagementSingleSchema> = {
-            siteId: this._pageContext.siteId,
-            webId: this._pageContext.webId,
+            siteId: Guid.normalizeLower(this._pageContext.siteId),
+            webId: Guid.normalizeLower(this._pageContext.webId),
             webTemplateId: this._pageContext.webTemplate
         };
 
         if (this._pageContext.groupId) {
-            engagementData.groupId = this._pageContext.groupId;
+            engagementData.groupId = Guid.normalizeLower(this._pageContext.groupId);
         }
 
         if (this._pageContext.listId) {
-            engagementData.listId = this._pageContext.listId;
+            engagementData.listId = Guid.normalizeLower(this._pageContext.listId);
         }
 
         if (this._pageContext.listBaseTemplate > -1) {
